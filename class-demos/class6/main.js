30f1bada25165389eb7796badb8ac8d0324c90ee
shingwh <ws2871@nyu.edu>
2026-09-22 15:30:14 -0400

    class 6 demo

---
+++ class-demos/class6/main.js
@@ -0,0 +1,14 @@
+// this is a comment
+
+alert('javascript!')
+// shows a popup like when you reload an unsaved page
+// alert always happen when the page loads
+
+console.log('log this info into the console')
+
+// window = the browser window
+//shorthand for waiting for webpage to load
+window.onload = () => {
+    console.log('page has loaded')
+
+}
